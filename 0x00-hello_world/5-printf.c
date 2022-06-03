a5389534cbbc85f5d302e6f6f1ff3fa88dc4791d
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-03 04:18:42 +0000

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Main entry for the code
+ *
+ * Return: Returns with success (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
