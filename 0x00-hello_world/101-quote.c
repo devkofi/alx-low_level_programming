e81cfbde1b893a4f0e05bf11aa7297ccd541545b
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-03 05:50:42 +0000

    Print to Std Error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry to the program
+ *
+ * Return: Return success (0)
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19");
+	return (1);
+}
