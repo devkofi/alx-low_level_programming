17977aa744c124d2e39f1f5a8414bdad489e0b8a
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-04 02:05:29 +0000

    Print in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry to the main program
+ *
+ * Return: Returns success (0)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 97; j <= 101; j++)
+	{
+		putchar(j);
+	}
+
+	putchar(10);
+	return (0);
+}
