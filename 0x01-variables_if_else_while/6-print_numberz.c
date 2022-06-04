dcbd372edbb6e3deb4e847f34a2efc3f490f2fce
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-04 01:34:16 +0000

    Update Print Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry to the main program
+ *
+ * Return: Returns success (0)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
