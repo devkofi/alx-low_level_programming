e2fa0a784e6493ce58952bfd548f83cb9ce9ced1
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-07 21:38:06 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 /**
  * main - Entry to the main program
  *
@@ -8,14 +7,19 @@
 
 int main(void)
 {
-	_putchar(95);
-	_putchar(112);
-	_putchar(117);
-	_putchar(116);
-	_putchar(99);
-	_putchar(104);
-	_putchar(97);
-	_putchar(114);
-	_putchar(10);
+	printChar();
 	return (0);
+}
+
+void printChar(void)
+{
+        _putchar(95);
+        _putchar(112);
+        _putchar(117);
+        _putchar(116);
+        _putchar(99);
+        _putchar(104);
+        _putchar(97);
+        _putchar(114);
+        _putchar(10);
 }
