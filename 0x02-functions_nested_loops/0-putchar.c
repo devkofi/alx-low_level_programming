49eaefb0abade875273a6203dc7c50f9fd34c4f8
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-07 21:46:20 +0000

    Betty Conformity

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,15 +11,18 @@ int main(void)
 	return (0);
 }
 
+/**
+ * printChar - Prints _putchar to stdout
+ */
 void printChar(void)
 {
-        _putchar(95);
-        _putchar(112);
-        _putchar(117);
-        _putchar(116);
-        _putchar(99);
-        _putchar(104);
-        _putchar(97);
-        _putchar(114);
-        _putchar(10);
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
 }
