747f55b01883e2bebb5563d0886a60c8e095d789
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-07 22:49:54 +0000

    Print Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints Lowercase alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		int j;
+
+		j = 97;
+
+		while (j <= 122)
+		{
+			_putchar(j);
+			j++;
+		}
+
+		_putchar(10);
+		i++;
+	}
