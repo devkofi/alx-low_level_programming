f28468ba396b09f33fe72a0dc97a80322f4a7708
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-07 22:21:40 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include "main.h"
 /**
- * main - Entry to the main program
- *
- * Return: Returns success (0)
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Prints Lowercase alphabets
  */
 void print_alphabet(void)
