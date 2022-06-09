23ac662bcb9ec2678272ad77fca4da96101a9b7b
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-09 21:29:16 +0000

    Use standard library

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,104 +1,37 @@
-#include "main.h"
-void print_fizz(void);
-void print_buzz(void);
-void refactor(int i);
-
+#include <stdio.h>
 /**
- * main - Entry to the program
+ * main - Entry to the main program
  *
- * Return: Returns success (0);
+ * Return: Returns success (0)
  */
 int main(void)
-{
-	fizz_buzz();
-	return (0);
-}
-/**
- * fizz_buzz - Weeding out the non-programmers
- *
- * Return: Returns void
- */
-void fizz_buzz(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i < 10)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			refactor(i);
-			if ((i % 3 != 0) && (i % 5 != 0))
-			{
-				_putchar('0' + i);
-			}
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
 		}
 		else
 		{
-			refactor(i);
-
-			if ((i % 3 != 0) && (i % 5 != 0))
-			{
-				if (i != 100)
-				{
-					_putchar('0' + (i / 10));
-					_putchar('0' + (i % 10));
-				}
-				else
-				{
-					_putchar(49);
-					_putchar(48);
-					_putchar(48);
-				}
-			}
+			printf("%d", i);
 		}
-		_putchar(32);
+
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
-
-	_putchar(10);
-}
-
-/**
- * print_fizz - Prints fizz
- *
- * Return: Returns void
- */
-void print_fizz(void)
-{
-	_putchar(70);
-	_putchar(105);
-	_putchar(122);
-	_putchar(122);
-}
-/**
- * print_buzz - Prints buzz
- *
- * Return: Returns void
- */
-void print_buzz(void)
-{
-	_putchar(66);
-	_putchar(117);
-	_putchar(122);
-	_putchar(122);
-}
-/**
- * refactor - Refactors code for printing fizzbuzz
- *
- * @i: Input
- */
-void refactor(int i)
-{
-			if ((i % 3 == 0) && (i % 5 == 0))
-			{
-				print_fizz();
-				print_buzz();
-			}
-			else if ((i % 3 == 0) && (i % 5 != 0))
-			{
-				print_fizz();
-			}
-			else if ((i % 5 == 0) && (i % 3 != 0))
-			{
-				print_buzz();
-			}
+	printf("\n");
+	return (0);
 }
