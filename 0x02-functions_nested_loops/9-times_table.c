1461e11031c12ab2f3239986bee3e7cebb312c26
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-09 02:08:49 +0000

    Times Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,69 @@
+#include "main.h"
+void details(int j, int k, int remainder, int divisor);
+/**
+ * times_table - Times table
+ *
+ * Return: returns void
+ */
+void times_table(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		int j;
+
+		j = 0;
+
+		while (j < 10)
+		{
+			int k;
+			int remainder;
+			int divisor;
+
+			k = i * j;
+			remainder = (k / 10);
+			divisor = (k % 10);
+
+			details(j, k, remainder, divisor);
+			j++;
+		}
+
+		_putchar(10);
+		i++;
+
+	}
+}
+/**
+ * details - Code refactor
+ * @j: Number of lines
+ * @k: Digits
+ * @remainder: last digit
+ * @divisor: lead digit
+ */
+void details(int j, int k, int remainder, int divisor)
+{
+	if (k > 9)
+	{
+		_putchar('0' + remainder);
+		_putchar('0' + divisor);
+	}
+	else
+	{
+		_putchar('0' + k);
+	}
+
+	if (j != 9 && k < 10)
+	{
+		_putchar(44);
+		_putchar(32);
+		_putchar(32);
+	}
+	else
+	{
+		_putchar(44);
+		_putchar(32);
+	}
+}
