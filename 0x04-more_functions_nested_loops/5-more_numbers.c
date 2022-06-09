4bf095d16af844073067894083955b37de524ec0
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-09 15:20:14 +0000

    Print from 0-14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times from 0-14
+ *
+ * return: Returns void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('0' + (j / 10));
+				_putchar('0' + (j % 10));
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
+		}
+
+		_putchar(10);
+		i++;
+	}
+}
