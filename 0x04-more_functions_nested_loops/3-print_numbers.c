e13440f623f5b4f05da926b2ff231def8c33eaf6
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-09 14:45:32 +0000

    Print from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0-9
+ *
+ * Return: Returns void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
