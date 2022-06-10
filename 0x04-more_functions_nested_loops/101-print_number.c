e0d78af2cd596ef77f0491b6e0856cbcd96a7652
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-10 06:09:19 +0000

    Update Print Integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -35,4 +35,11 @@ void print_number(int n)
 		_putchar('0' + ((n % 100) / 10));
 		_putchar('0' + ((n % 100) % 10));
 	}
+	else
+	{
+		_putchar('0' + (n / 1000));
+		_putchar('0' + (n % 1000) / 100);
+		_putchar('0' + ((((n % 1000) % 100) / 10)));		
+		_putchar('0' + (((((n % 1000) % 100) % 10))));
+	}
 }
