9dd4d117e60ce63d8fe45e6ac797b41ebdb94b58
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-10 03:55:30 +0000

    Print Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle using #
+ *
+ * @size: Size of the triangle
+ *
+ * Return: Returns void
+ */
+void print_triangle(int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		int j;
+
+		for (j = size - i; j > i; j--)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -21,3 +21,5 @@ void print_diagonal(int n);
 void print_square(int size);
 
 void fizz_buzz(void);
+
+void print_triangle(int size);
