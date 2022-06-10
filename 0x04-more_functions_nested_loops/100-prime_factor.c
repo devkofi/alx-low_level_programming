13359545c3c3f6549d7fa7f860ef2bb49049dd9d
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-10 05:41:14 +0000

    Print Integers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main - Entry to the main program
+ *
+ * Return: Returns success (0)
+ */
+int main(void)
+{
+	int prime;
+
+	prime = 612852475143;
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer
+ *
+ * @n: Input
+ *
+ * Return: Returns void
+ */
+void print_number(int n)
+{
+	if (n < 0 && n > -10)
+	{
+		_putchar(45);
+		_putchar('0' + (-1 * n));
+
+	}
+	else if (n <= -10 && n > -100)
+	{
+		_putchar(45);
+		_putchar('0' + ((n / 10) * -1));
+		_putchar('0' + (-n % 10));
+	}
+	else if (n >= 0 && n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	else if (n >= 100 && n < 1000)
+	{
+
+	}
+}
