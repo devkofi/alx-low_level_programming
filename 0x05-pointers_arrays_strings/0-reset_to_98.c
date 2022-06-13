dc332f5b1670f5a8a76066050920f7ecfedbb3bc
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-13 13:44:29 +0000

    Reset Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Reset pointer to 98
+ *
+ * Return - Returns void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;	
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,3 @@
-void _putchar(int n);
+void _putchar(char c);
 
 void reset_to_98(int *n);
