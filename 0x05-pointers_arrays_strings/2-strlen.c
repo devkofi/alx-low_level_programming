1f3d62a6f58b4c90180d51260d47e5b6b3bbee21
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-13 16:26:25 +0000

    Print length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strlen - Gets the length of a string
+ *
+ * @s: Pointer to a string
+ *
+ * Return: Returns void
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
