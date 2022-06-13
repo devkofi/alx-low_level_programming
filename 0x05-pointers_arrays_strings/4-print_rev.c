6f7da9a274f8bf222c67fd8206bb68ec212777a7
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-13 16:50:18 +0000

    Print Reversed String

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "string.h"
+/**
+ * print_rev - Prints to standard output in reverse
+ *
+ * @s: Pointer to a char
+ *
+ * Return: Returns void
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+
+	length = strlen(s);
+
+	for (i = length; i >= 0; i--)
+	{
+		int c;
+
+		c = s[i];
+
+		_putchar(c);
+	}
+
+	_putchar(10);
+}
