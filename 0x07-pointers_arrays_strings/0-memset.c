3b75fd0d31e5518d199b0b8c8257409726989645
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-20 16:28:39 +0000

    Fill Memory with byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _memset - Copies byte to memory
+ *
+ * @s: Pointer to a char 
+ * @b: Duplicate character
+ * @size: Copy size
+ *
+ * Return: Returns a pointer to char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
