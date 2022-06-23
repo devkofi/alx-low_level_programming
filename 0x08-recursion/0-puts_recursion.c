2440faf47e4d27a05ecd6aaa730508164a081b79
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-23 12:53:37 +0000

    Print String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string followed by a new line
+ *
+ * @s: Pointer to a char
+ *
+ * Return: Returns void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
