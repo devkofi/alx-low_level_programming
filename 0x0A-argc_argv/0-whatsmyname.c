a0f5fef644c10866b939feceef83725aa8d541f5
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-25 06:16:37 +0000

    Print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry to the main program
+ *
+ * @argc: Argument count
+ * @argv: Argument vector which is identical to one dimensional array
+ *
+ * Return: Returns success 0
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
