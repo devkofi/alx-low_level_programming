23fe16416f06da0a68590fddfd72637c1bf20fff
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-29 14:28:01 +0000

    Change Pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int j;
 	int **array;
 
-	array = (int **) malloc(width * sizeof(int));
+	array = (int **) malloc(width * sizeof(int *));
 
 	if (array == NULL)
 	{
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < width; i++)
 	{
-		array[i] = (int *) malloc(height * sizeof(int *));
+		array[i] = (int *) malloc(height * sizeof(int));
 
 		for (j = 0; j < height; j++)
 		{
