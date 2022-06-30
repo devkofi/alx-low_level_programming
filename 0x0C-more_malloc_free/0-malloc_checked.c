5640ca2fd7b836debb3edde3ad6644b8f578c871
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-30 16:27:23 +0000

    Create memory space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - Allocate Memory
+ *
+ * @b: Size to allocate
+ *
+ * Return: Returns void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = (void *) malloc(b + 1);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
