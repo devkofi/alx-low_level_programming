e721561611ee943a875c3767c351158097f14f17
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-30 17:47:29 +0000

    Increase length

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,10 @@
 int *array_range(int min, int max)
 {
 	int *ptr;
+	int i;
+	int counter;
 
+	counter = 0;
 	ptr = (int *) malloc((max - min + 1) * sizeof(int));
 
 	if (ptr == NULL)
@@ -23,6 +26,12 @@ int *array_range(int min, int max)
 	if (min > max)
 	{
 		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		*(ptr + counter) = i;
+		counter++;
 	}
 
 	return (ptr);
