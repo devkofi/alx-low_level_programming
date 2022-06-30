6b64d554a9c170938606530ef52cb8631b7e1b93
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-06-30 17:02:42 +0000

    String concatenate

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - String concatenation
+ *
+ * @s1: First String
+ * @s2: Second String
+ * @n: Memory Size
+ *
+ * Return: Returns pointer to a void
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	int j;
+	char *array;
+	int length;
+	int length_s1;
+	int counter;
+
+	counter = 0;
+	length_s1 = strlen(s1);
+	length = length_s1 + n + 1;
+	array = (char *) malloc(length * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	if (n > strlen(s2))
+	{
+		length = length_s1 + strlen(s2) + 1;
+	}
+
+	for (i = 0; i < length_s1; i++)
+	{
+		*(array + i) = *(s1 + i);
+	}
+
+	for (j = length_s1; j < length; j++)
+	{
+		*(array + j) = *(s2 + counter);
+		counter++;
+	}
+
+	*(array + length) = '\0';
+
+	return (array);
+}
